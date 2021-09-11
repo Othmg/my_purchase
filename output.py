27ba852d59957323d92bb81c9362abbe063f3ba1
[ogacem] <[ogacem@outlook.com]>
2021-09-11 15:31:46 +0200

    output class

--- output.py
+++ output.py
@@ -4,7 +4,6 @@ from optimizer import Optimized
 import json 
 from datetime import datetime
 
-### to build: checks + optimizer output ####
 """
 Values in input dict should all be float (exception for item as string) and no negative values, rates should be <1
 - int_rate: only cost of debt, does not include repayment of capital
@@ -33,47 +32,46 @@ the methods to retrieve the dicts required a time input.
 returns a dictionary type amount,time,category. if you need just the amount you can use .amnt
 """
 
+class output:
+  def __init__(self, input_dict: dict, time:float) -> None:
+      self.input_dict = input_dict
+      self.revenue, self.exp_input,self.item,self.price,self.start_capital,self.time_until_purchase,self.loan,self.int_rate,self.maintenance_rate = self.input_dict.values()
+      self.book = Bookings(self.revenue, self.exp_input,self.item,self.price,self.start_capital,self.time_until_purchase,self.loan,self.int_rate,self.maintenance_rate)
+      self.time = time
+
+  def basic_json(self):
+    output_json ={ 
+    "income": self.book.inc_acc(self.time).amnt, 
+    "expense": self.book.exp_acc(self.time).amnt, 
+    "capital": self.book.cash_acc(self.time).amnt
+    } 
+        
+    # Serializing json  
+    return json.dumps(output_json, indent = 4) 
+
+  def ranked_json(self):
+    ranked_time = 0
+    opt = Optimized(self.book.inc_acc(ranked_time), self.book.exp_acc(ranked_time), self.book.cash_acc(ranked_time).amnt,self.price, self.loan,self.time_until_purchase)
+    return opt.sorted_dict_json()
+
+
+out_1 = output(input_dict,1)
+print(out_1.ranked_json())
+##TODO if stmt: if user too pessimistic, optimal values should be sorted by largest change first, if user is pessimistic then smallest change first
+
+
+
+
+
 
 def diff_month(d1, d2):
     return (d1.year - d2.year) * 12 + d1.month - d2.month
 
 assert diff_month(datetime(2010,10,1), datetime(2010,9,1)) == 1
 
-revenue, exp_input,item,price,start_capital,time_until_purchase,loan,int_rate,maintenance_rate = input_dict.values()
 
-book = Bookings(revenue, exp_input,item,price,start_capital,time_until_purchase,loan,int_rate,maintenance_rate)
 
-time = 3
-income = book.inc_acc(time)
-expense = book.exp_acc(time)
 
-#in json
-output_json ={ 
-  "income": book.inc_acc(time).amnt, 
-  "expense": book.exp_acc(time).amnt, 
-  "capital": book.cash_acc(time).amnt
-} 
-      
-# Serializing json  
-json_object = json.dumps(output_json, indent = 4) 
-print(json_object)
-
-"""
-The Optimized function returns optimised expense level, time_until_purchase and purchase price
-as well as the gap with the user input.
-use 0 as default time values
-"""
-opt = Optimized(book.inc_acc(0),book.exp_acc(0),start_capital,price,time_until_purchase)
-#exemple
-# print(opt._expense())
-# print(opt._expense_gap())
-# print(opt._expense_gap_pct())
-
-gap_list = [opt._expense_gap_pct(),opt._time_until_purchase_gap_pct(),opt._purchase_gap_pct()]
-gap_list = sorted(gap_list)
-print(gap_list)
-
-##TODO have json output for optimised including new values ordered by pct change?
 
 
 
