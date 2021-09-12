88723a91163c079aacbacf5f9bd998fe67720ce4
[ogacem] <[ogacem@outlook.com]>
2021-09-12 21:41:56 +0200

    added testing inputs

--- output.py
+++ output.py
@@ -22,6 +22,33 @@ input_dict = {
             'maintenance_rate':0.1
             }
 
+input_dict_2 = {
+            'revenue':10000.0,
+            'exp_input':9000.0,
+            'item':'car',
+            'price':30000.0,
+            'start_capital':0.0,
+            'time_until_purchase':6.0,
+            'loan':0.0,
+            'int_rate':0.0,
+            'maintenance_rate':0.0
+            }
+
+
+
+input_dict_3 = {
+            'revenue':5000.0,
+            'exp_input':3000.0,
+            'item':'car',
+            'price':30000.0,
+            'start_capital':0.0,
+            'time_until_purchase':6.0,
+            'loan':0.0,
+            'int_rate':0.0,
+            'maintenance_rate':0.0
+            }
+
+
 """
 All values are passed into the Bookings class which takes care of the accounting manipulation
 the methods to retrieve the dicts required a time input. 
@@ -36,6 +63,7 @@ class output:
   def __init__(self, input_dict: dict, time:float) -> None:
       self.input_dict = input_dict
       self.revenue, self.exp_input,self.item,self.price,self.start_capital,self.time_until_purchase,self.loan,self.int_rate,self.maintenance_rate = self.input_dict.values()
+
       self.book = Bookings(self.revenue, self.exp_input,self.item,self.price,self.start_capital,self.time_until_purchase,self.loan,self.int_rate,self.maintenance_rate)
       self.time = time
 
@@ -54,14 +82,21 @@ class output:
     opt = Optimized(self.book.inc_acc(ranked_time), self.book.exp_acc(ranked_time), self.book.cash_acc(ranked_time).amnt,self.price, self.loan,self.time_until_purchase)
     return opt.sorted_dict_json()
 
+  def test_view(self):
+    ranked_time = 0
+    opt = Optimized(self.book.inc_acc(ranked_time), self.book.exp_acc(ranked_time), self.book.cash_acc(ranked_time).amnt,self.price, self.loan,self.time_until_purchase)
+    print('usr exp',self.exp_input)
+    print('opt exp',opt._expense())
+    print('exp gap',opt._expense_gap())
+    print('exp gap pct',opt._expense_gap_pct())
+    print('is optimist',opt.is_optimist())
+    print('sorted gap in pct',opt._sorted_gap())
+    print('sorted dict',opt.sorted_dict())
+    
 
-out_1 = output(input_dict,1)
-print(out_1.ranked_json())
+out_1 = output(input_dict_2,1)
+print(out_1.test_view())
 ##TODO if stmt: if user too pessimistic, optimal values should be sorted by largest change first, if user is pessimistic then smallest change first
-
-
-
-
 
 
 def diff_month(d1, d2):
